b3cd09fd77458a2fc04d18a9ba6419b700a163b4
MorganLeskerpit-ECN <97032677+MorganLeskerpit-ECN@users.noreply.github.com>
2023-05-09 16:09:18 +0900

    working on logistic series

--- main.py
+++ main.py
@@ -1,16 +1,68 @@
-# This is a sample Python script.
+"""
+Author: LESKERPIT Morgan レスカピット　モルガン
+Keio ID: 8222300
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+COGNITIVE ROBOTICS ASSIGNMENT
+"""
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Study of the logistic map
+
+def f_logistic(x: float, r: float) -> float:
+    """
+
+    :param x: X value at the step n
+    :param r: r value
+    :return: X value at the step n + 1
+    """
+    return r * x * (1 - x)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Parameters
+r = 2
+U0 = 0.1
+N = 1000
+
+U_series = [U0]
+
+# Plotting the logistic function
+x_list = np.arange(0, 1, 0.01)
+y_list = np.array([f_logistic(x, r) for x in x_list])
+
+plt.plot(x_list, y_list)
+plt.plot([x_list[0], x_list[-1]], [x_list[0], x_list[-1]], color='blue')
+
+for n in range(0, N):
+    Un1 = f_logistic(U_series[n], r)
+    U_series.append(Un1)
+
+x_showUSeriesStep = [U_series[0]]
+y_showUSeriesStep = [U_series[0]]
+for n in range(1, len(U_series)):
+    x_showUSeriesStep.append(U_series[n-1])
+    y_showUSeriesStep.append(U_series[n])
+
+    x_showUSeriesStep.append(U_series[n])
+    y_showUSeriesStep.append(U_series[n])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+plt.plot(x_showUSeriesStep, y_showUSeriesStep, linestyle='-', marker='o')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+plt.show()
+
+def update(frame, frame_times):
+    frame_times[frame] = time.perf_counter()
+    y2= 1000*np.diff(frame_times)
+    y2_avg = np.nanmean(y2)
+
+    lineL.set_data(t[frame], y[frame])
+    lineR.set_data(t[frame], y2)
+    titL.set_text(f"N={frame}")
+    titR.set_text(f"AVG = {y2_avg:.1f} ms ({1000/y2_avg:.1f} fps")
+
+    rescale = False
+
+    #...
